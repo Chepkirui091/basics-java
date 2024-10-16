a696026987c2817b7fecd5e5d6a451a92fa0f7d5
Chepkirui91 <chepkiruidaphne91@gmail.com>
2024-10-16 13:10:18 +0300

    j

---
+++ src/FirstJavaClass.java
@@ -0,0 +1,16 @@
+public class FirstJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        //declare variable
+        int firstVariable;
+
+        //initialize a variable
+        firstVariable = 10;
+        int secondVariable = 20;
+        int total = firstVariable + secondVariable;
+
+        System.out.println("firstVariable:" + firstVariable);
+        System.out.println("Total:" + total);
+    }
+}
