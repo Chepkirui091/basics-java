5cb3593d8e027a1c1a92af047a976b1cd5538be3
Chepkirui91 <chepkiruidaphne91@gmail.com>
2024-10-17 15:49:49 +0300

    operators

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -12,5 +12,44 @@ public class FirstJavaClass {
 
         System.out.println("firstVariable:" + firstVariable);
         System.out.println("Total:" + total);
+
+        // OPERATORS
+         /*
+        Arithmetic operators:+ - * / % ++ --
+        Relational operators: == != > < >= <=
+        Logical operators: && || !
+        Assignment operators: = += -+ /+ %=
+        conditional operators: ? :
+
+         */
+        int a=10, b=5, c=50, d=25;
+        System.out.println("a+b= "+ (a+b));
+        System.out.println("a-b= "+ (a-b));
+        System.out.println("a*b= "+ (a*b));
+        System.out.println("c/b= "+ (c/b));
+        System.out.println("a%b= "+ (b%a));
+
+        //Increment operators
+        {
+            int x=100;
+            int y=x++;  //postincrement: value is assigned or used in an expression first and then incremented
+            int z = ++x;  //preincrement: value is incremented first and then assigned or used in an expression
+            System.out.println("value of x= " + x);
+            System.out.println("value of y= " + y);
+            System.out.println("value of z= " + z);
+
+        }
+        {
+            int x=100;
+            int y=x--;  //postincrement: value is assigned or used in an expression first and then incremented
+            int z = --x;  //preincrement: value is incremented first and then assigned or used in an expression
+            System.out.println("value of x= " + x);
+            System.out.println("value of y= " + y);
+            System.out.println("value of z= " + z);
+
+        }
     }
+
 }
+
+
